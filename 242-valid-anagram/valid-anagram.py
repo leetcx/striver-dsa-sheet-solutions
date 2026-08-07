bdbb7488b2d259fec09b8c10ea5cad040b1b4523
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-07 12:18:26 +0530

    Time: 15 ms (44.49%) | Memory: 19.4 MB (46.19%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq1={}
+        freq2={}
+
+        for i in s:
+            if i in freq1:
+                freq1[i]+=1
+            else:
+                freq1[i]=1
+        for i in t:
+            if i in freq2:
+                freq2[i]+=1
+            else:
+                freq2[i]=1
+        if freq1==freq2:
+            return True
+        else:
+            return False
+        
