1b891fa20ff5899916f2cee5ac57ec58fc6542c6
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-10 13:37:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (33.39%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -18,12 +18,12 @@ class Solution:
                 l2=l2.next
             tail=tail.next
 
-        while l1:
+        if l1:
             tail.next=l1
             tail=tail.next
             l1=l1.next
         
-        while l2:
+        if l2:
             tail.next=l2
             tail=tail.next
             l2=l2.next
