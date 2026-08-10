d9ee57dc78e9bcf48786e78fc893f8bc45981109
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-11 03:02:59 +0530

    Time: 127 ms (28.46%) | Memory: 19.7 MB (36.91%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,35 +1,36 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        n=len(s)
+        p=len(t)
         set1={}
-
         for i in t:
             if i in set1:
                 set1[i]+=1
             else:
                 set1[i]=1
-        p=len(s)
-        d=len(t)
-        minlen=float("inf")
-        ceta={}
-        ans=""
         have=0
+        freq={}
+        minlen=float("inf")
         left=0
-        for i in range(p):
-            if s[i] in ceta:
-                ceta[s[i]]+=1
+        ans=""
+        for i in range(n):
+            if s[i] in freq:
+                freq[s[i]]+=1
             else:
-                ceta[s[i]]=1
-            if s[i] in set1 and ceta[s[i]] <= set1[s[i]]:
-                have=have+1
-            while have==d:
+                freq[s[i]]=1
+            if s[i] in set1 and freq[s[i]]==set1[s[i]]:
+                have+=1
+            while have==len(set1):
                 if i-left+1<minlen:
-                    ans=s[left:i+1]
                     minlen=i-left+1
+                    ans=s[left:i+1]
+                    
                 leftchar=s[left]
-                ceta[leftchar]-=1
-                if leftchar in set1 and ceta[leftchar] < set1[leftchar]:
+                freq[leftchar]-=1
+                if leftchar in set1 and set1[leftchar]>freq[leftchar]:
                     have-=1
                 left+=1
         return ans
+
 
 
