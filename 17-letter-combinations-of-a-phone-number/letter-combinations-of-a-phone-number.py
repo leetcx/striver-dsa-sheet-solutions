e796b4ac4eb085978588c15973a1922696c14970
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-13 14:12:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (9.40%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -4,15 +4,16 @@ class Solution:
         n=len(digits)
         phone = {"2": "abc","3": "def","4": "ghi","5": "jkl","6": "mno","7": "pqrs","8": "tuv", "9": "wxyz"}
         ans=[]
+        n=len(digits)
 
-        def backtrack(digits,n,i):
+        def back(i):
             if i==n:
                 ans.append("".join(temp))
                 return
-            choice=phone[digits[i]]
-            for j in range(len(choice)):
-                temp.append(choice[j])
-                backtrack(digits,n,i+1)
+            strop=phone[digits[i]]
+            for j in range(len(strop)):
+                temp.append(strop[j])
+                back(i+1)
                 temp.pop()
-        backtrack(digits,n,0)
+        back(0)
         return ans
