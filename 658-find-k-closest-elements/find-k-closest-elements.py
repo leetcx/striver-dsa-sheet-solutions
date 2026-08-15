63dbc6a687332f0a8dd739ba7c035fd1de6a344b
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-15 12:05:55 +0530

    Time: 27 ms (26.97%) | Memory: 21.6 MB (19.03%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,13 +1,18 @@
 class Solution:
-    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        n=len(arr)
-        low=0
-        high=n-k
+    def findClosestElements(self, nums: List[int], k: int, x: int) -> List[int]:
+        n=len(nums)
+        ans=[]
+       
+        for i in range(n):
+            ans.append((nums[i],abs(x-nums[i])))
+        ans.sort(key=lambda x:x[1])
+        subarr=ans[0:k]
+        po=[]
+        for i in range(len(subarr)):
+            po.append(subarr[i][0])
+        po.sort()
+        return po
 
-        while low<high:
-            mid=(low+high)//2
-            if x-arr[mid] <= arr[mid+k]-x:
-                high=mid
-            else:
-                low=mid+1
-        return arr[low:high+k]
+        
+
+
