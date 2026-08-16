a716a92dc49f7dfa714586010e6b3c3a00d259f3
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-16 22:15:57 +0530

    Time: 11 ms (57.73%) | Memory: 19.6 MB (13.65%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,28 +1,22 @@
 class Solution:
-    def combinationSum2(self, cand: List[int], target: int) -> List[List[int]]:
-        cand.sort()
-        ans = []
-        temp = []
-
-        def backtrack(i, total):
-            if total == target:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        ans=[]
+        temp=[]
+        def backtrack(i,sum):
+            nonlocal ans
+            nonlocal temp
+            if sum==target:
                 ans.append(temp.copy())
                 return
-
-            if i == len(cand) or total > target:
-                return
-
-            for j in range(i, len(cand)):
-
-                if j > i and cand[j] == cand[j - 1]:
+            for j in range(i,len(candidates)):
+                if j>i and candidates[j]==candidates[j-1]:
                     continue
-
-                if total + cand[j] > target:
-                    break
-
-                temp.append(cand[j])
-                backtrack(j + 1, total + cand[j])
-                temp.pop()
-
-        backtrack(0, 0)
+                if sum+candidates[j] <=target:
+                    
+                    temp.append(candidates[j])
+                    backtrack(j+1,sum+candidates[j])
+                    temp.pop()
+        backtrack(0,0)
         return ans
+
