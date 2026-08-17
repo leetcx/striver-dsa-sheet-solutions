521d2b2ca48dad8d87b7d9a9c2696746b5f397a1
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-17 14:05:09 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (92.78%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.py
@@ -6,47 +6,45 @@
 #         self.right = right
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        if root==  None:
-            return 0
+        if root==None:
+            return None
         st=[]
         st2=[]
         node=root
         de=root
         def getsmall():
+            nonlocal st
             nonlocal node
             while node:
                 st.append(node)
                 node=node.left
             p=st.pop()
             small=p.right
+           
             while small:
                 st.append(small)
                 small=small.left
             return p
         def getbig():
+            nonlocal st2
             nonlocal de
             while de:
                 st2.append(de)
                 de=de.right
-            i=st2.pop()
-            big=i.left
+            q=st2.pop()
+            big=q.left
+            
             while big:
                 st2.append(big)
                 big=big.right
-            return i
-        m=getsmall()
-        q=getbig()
-
-        while m != q:
-            if m.val + q.val == k:
+            return q
+        i=getsmall()
+        j=getbig()
+        while i!=j:
+            if i.val+j.val==k:
                 return True
-
-            elif m.val + q.val < k:
-                m = getsmall()
-
+            if i.val+j.val>k:
+                j=getbig()
             else:
-                q = getbig()
-
+                i=getsmall()
         return False
-        
-            
