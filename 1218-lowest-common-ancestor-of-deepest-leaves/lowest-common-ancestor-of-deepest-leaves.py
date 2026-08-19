d6bb0c43a4ce402fe16ee1384a43909f5a9eed39
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-19 21:21:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (93.93%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.py
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.py
@@ -8,15 +8,15 @@ class Solution:
     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         def deep(node):
             if node==None:
-                return (0,None)
+                return 0,None
             leftdepth,leftnode=deep(node.left)
             rightdepth,rightnode=deep(node.right)
 
             if leftdepth==rightdepth:
-                return (leftdepth+1,node)
-            if leftdepth>rightdepth:
-                return (leftdepth+1,leftnode)
+                return ((leftdepth+1),node)
+            elif leftdepth>rightdepth:
+                return ((leftdepth+1),leftnode)
             else:
-                return (rightdepth+1,rightnode)
-        return deep(root)[1]
-            
+                return ((rightdepth+1),rightnode)
+        depth,ans=deep(root)
+        return ans
