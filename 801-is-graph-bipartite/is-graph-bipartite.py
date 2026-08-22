04648eff54f1e3d0d69e326e5ca9063cba4c4eb6
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-22 23:28:10 +0530

    Time: 7 ms (9.05%) | Memory: 19.8 MB (53.20%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,20 +1,19 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        p=len(graph)
+        p= len(graph)
         colors=[-1] * p
-        res=True
+        res= True
         def dfs(colors,node,c,graph):
             nonlocal res
-            colors[node]=c
-            for j in range(len(graph[node])):
+            for j in  range(len(graph[node])):
                 neigh=graph[node][j]
-                if colors[neigh] !=-1 and colors[neigh]==c:
+                if colors[neigh]==c:
                     res=False
-                if colors[neigh]== -1:
-                    dfs(colors,neigh,(1-c),graph)
-
-                    
+                if colors[neigh]==-1:
+                    colors[neigh]=1-c
+                    dfs(colors,neigh,1-c,graph)
         for i in range(p):
             if colors[i]==-1:
+                colors[i]=0
                 dfs(colors,i,0,graph)
         return res
