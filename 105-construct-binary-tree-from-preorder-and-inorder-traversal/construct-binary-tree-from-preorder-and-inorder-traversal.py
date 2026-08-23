bb1aaf6900587ae4815564776731bd486363b792
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-24 00:49:50 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (88.68%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,20 +6,22 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        seti={}
-        for i in range (len(inorder)):
-            seti[inorder[i]]=i
+        set1={}  
+        for i in range(len(inorder))  :
+            set1[inorder[i]] =i
         preindex=0
-        def build(low,high):
+        def const(low,high):
             nonlocal preindex
+            nonlocal set1
             if low>high:
-                return None
+                return
             val=preorder[preindex]
             preindex+=1
-            p=seti[val]
-            root=TreeNode(val)
+            node=TreeNode(val)
+            p=set1[val]
+            node.left=const(low,p-1)
+            node.right=const(p+1,high)
+            return node
+        return const(0,len(inorder)-1)
+       
 
-            root.left=build(low,p-1)
-            root.right=build(p+1,high)
-            return root
-        return build(0,len(inorder)-1)
