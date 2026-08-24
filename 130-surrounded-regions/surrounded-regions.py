0360be31915ab79b1d4fcf81697bec3fa83737b2
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-24 23:32:23 +0530

    Time: 4 ms (67.46%) | Memory: 22.2 MB (91.26%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -5,36 +5,40 @@ class Solution:
         """
         x=[-1,1,0,0]
         y=[0,0,-1,1]
-        n=len(board)
-        m=len(board[0])
-        def dfs(board,m,n,i,j,x,y):
-            board[i][j]='#'
-            for k in range(4):
-                row=i+x[k]
-                col=j+y[k]
-                if valid(row,col,m,n) and board[row][col] == 'O':
-                    dfs(board,m,n,row,col,x,y)
-        def valid(i,j,m,n):
-            if i<0 or i>=n or j<0 or j>=m:
+        m=len(board)
+        n=len(board[0])
+        visited=[[False]*n for _ in range(m)]
+        def valid(r,c,m,n):
+            if r<0 or r>=m or c<0 or c>=n:
                 return False
             return True
-        for j in range(m):
-            if board[0][j]=='O':
-                dfs(board,m,n,0,j,x,y)
-        for j in range(m):
-            if board[n-1][j]=='O':
-                dfs(board,m,n,n-1,j,x,y)
+
+        def dfs(row,col,board):
+            for k in range(4):
+                r=row+x[k]
+                c=col+y[k]
+                if valid(r,c,m,n) and board[r][c]=='O' and visited[r][c]==False:
+                    visited[r][c]=True
+                    dfs(r,c,board)
+
         for i in range(n):
+            if board[0][i]=='O':
+                visited[0][i]=True
+                dfs(0,i,board)
+        for j in range(n):
+            if board[m-1][j]=='O':
+                visited[m-1][j]=True
+                dfs(m-1,j,board)
+        for i in range(m):
             if board[i][0]=='O':
-                dfs(board,m,n,i,0,x,y)
-        for i in range(n):
-            if board[i][m-1]=='O':
-                dfs(board,m,n,i,m-1,x,y)
-        for i in range(n):
-            for j in range(m):
-                if board[i][j]=='#':
-                    board[i][j]='O'
-                else:
-                    board[i][j]='X'
-
-
+                visited[i][0]=True
+                dfs(i,0,board)
+        for j in range(m):
+            if board[j][n-1]=='O':
+                visited[j][n-1]=True
+                dfs(j,n-1,board)
+        for l in range(m):
+            for t in range(n):
+                if board[l][t]=='O' and visited[l][t]==False:
+                    board[l][t]='X'
+        
