fb453d264e7a74c32bf9a1ef9bcadd4134d09fcc
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-24 15:09:43 +0530

    Time: 239 ms (69.96%) | Memory: 21.6 MB (47.84%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,29 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        n=len(grid)
-        m=len(grid[0])
-        visited = [[False] * m for _ in range(n)]
         x=[-1,1,0,0]
         y=[0,0,-1,1]
-        res=0
-        
-        
-        def dfs(grid,i,j,visited,x,y,m,n):
-            visited[i][j]=True
-            for k in range(0,4):
-                row=i+x[k]
-                col=j+y[k]
-                if valid(row,col,m,n) and grid[row][col]=='1' and visited[row][col]==False:
-                    dfs(grid,row,col,visited,x,y,m,n)
-        def valid(i,j,m,n):
-            if i<0 or i>=n or j<0 or j>=m:
+        m=len(grid)
+        n=len(grid[0])
+        visited=[[False] *n for _ in range(m)]
+        p=0
+        def valid(r,c,m,n):
+            if r<0 or r>=m or c<0 or c>= n:
                 return False
             return True
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j]== '1' and visited[i][j]==False:
-                    dfs(grid,i,j,visited,x,y,m,n)
-                    res+=1
-        return res
+        def dfs(row,col,visited,grid):
+            visited[row][col]=True
+            for k in range(4):
+                r=row+x[k]
+                c=col+y[k]
+                if valid(r,c,m,n) and visited[r][c]==False and grid[r][c]=='1':
+                    dfs(r,c,visited,grid)
 
-
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]=='1' and visited[i][j]==False:
+                    dfs(i,j,visited,grid)
+                    p+=1
+        return p
