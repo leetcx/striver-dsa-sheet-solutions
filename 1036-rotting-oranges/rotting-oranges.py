1a38600526425e71182b87e4c6d3f2fc29598438
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-24 22:14:11 +0530

    Time: 5 ms (22.80%) | Memory: 19.4 MB (12.70%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,41 +1,43 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        m=len(grid)
-        n=len(grid[0])
         x=[-1,1,0,0]
         y=[0,0,-1,1]
-        q=deque()
         fresh=0
+        m=len(grid)
+        n=len(grid[0])
+        q=deque()
+        visited=[[False]*n for _ in range(m)]
         time=0
         for i in range(m):
             for j in range(n):
                 if grid[i][j]==2:
                     q.append((i,j))
-                elif grid[i][j]==1:
+                    visited[i][j]=True
+                if grid[i][j]==1:
                     fresh+=1
-        def valid(i,j,m,n):
-            if i<0 or i>=m or j<0 or j>=n:
+        def valid(r,c,m,n):
+            if r<0 or r>=m or c<0 or c>=n:
                 return False
             return True
-        while q and fresh>0:
+        while q:
             p=len(q)
-            while p:
-                i,j=q.popleft()
-                for k in range(0,4):
-                    row=i+x[k]
-                    col=j+y[k]
-                    if valid(row,col,m,n) and grid[row][col]==1:
-                        q.append((row,col))
-                        grid[row][col]=-2
+            for i in range(p):
+                row,col=q.popleft()
+               
+                for k in range(4):
+                    r=row+x[k]
+                    c=col+y[k]
+                    if valid(r,c,m,n) and visited[r][c]==False and grid[r][c]==1:
+                        q.append((r,c))
                         fresh-=1
-                p-=1
-            time+=1
+                        visited[r][c]= True
+            if q:
+                time+=1
+                    
+            
         if fresh>0:
             return -1
         return time
-               
 
-
-
-
+        
