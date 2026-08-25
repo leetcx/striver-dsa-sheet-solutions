dc6421fc3c20d34b22f6087d6f2d6b6acf6aeb18
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-26 01:33:37 +0530

    Time: 253 ms (37.25%) | Memory: 21.5 MB (72.58%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,26 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        x=[-1,1,0,0]
-        y=[0,0,-1,1]
         m=len(grid)
         n=len(grid[0])
-        visited=[[False] *n for _ in range(m)]
-        p=0
+        res=0
+        x=[-1,1,0,0]
+        y=[0,0,-1,1]
+        visited=[[False]*n for _ in range(m)]
         def valid(r,c,m,n):
-            if r<0 or r>=m or c<0 or c>= n:
+            if r<0 or r>=m or c<0 or c>=n:
                 return False
             return True
-        def dfs(row,col,visited,grid):
+        def dfs(grid,row,col,visited):
             visited[row][col]=True
             for k in range(4):
                 r=row+x[k]
                 c=col+y[k]
                 if valid(r,c,m,n) and visited[r][c]==False and grid[r][c]=='1':
-                    dfs(r,c,visited,grid)
-
+                    dfs(grid,r,c,visited)
         for i in range(m):
             for j in range(n):
                 if grid[i][j]=='1' and visited[i][j]==False:
-                    dfs(i,j,visited,grid)
-                    p+=1
-        return p
+                    dfs(grid,i,j,visited)
+                    res+=1
+        return res
