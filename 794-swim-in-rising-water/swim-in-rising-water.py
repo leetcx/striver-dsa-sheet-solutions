9c8688135c3084d49f1e6720a3c09b3dd78d5f8a
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-25 21:29:47 +0530

    Time: 20 ms (75.61%) | Memory: 19.7 MB (91.60%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -1,30 +1,29 @@
 import heapq
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
+        n=len(grid)
         x=[-1,1,0,0]
         y=[0,0,-1,1]
-        m=len(grid)
-        n=len(grid[0])
-        res=[[float('inf') for _ in range(n)] for _ in range(m)]
+        q=[]
+        heapq.heappush(q,(grid[0][0],0,0))
+        res = [[float('inf')] * n for _ in range(n)]
         res[0][0]=grid[0][0]
-        pq=[]
-        heapq.heappush(pq,(grid[0][0],0,0))
-        def valid(m,n,i,j):
-            if i<0 or i>= m or j<0 or j>=n:
+        def valid(r,c,n):
+            if r<0 or r>=n or c<0 or c>=n:
                 return False
             return True
-        while pq:
-            money,row,col=heapq.heappop(pq)
-            if money > res[row][col]:
+        while q:
+            dist,row,col=heapq.heappop(q)
+            if row==n-1 and col==n-1:
+                return dist
+            if dist > res[row][col]:
                 continue
             for k in range(4):
                 r=row+x[k]
                 c=col+y[k]
-                if  not valid(m,n,r,c):
-                    continue
-                adji=max(money,grid[r][c])
-                if adji < res[r][c]:
-                    res[r][c]=adji
-                
-                    heapq.heappush(pq,(adji,r,c))
-        return res[m-1][n-1]
+                if valid(r,c,n):
+                    t=max(dist,grid[r][c])
+                    if t< res[r][c]:
+                        res[r][c]=t
+                        heapq.heappush(q,(t,r,c))
+        return res[n-1][n-1]
