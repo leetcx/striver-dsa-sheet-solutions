8bd3900e704f5cbf2e15b558e534bbb896700291
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-27 02:51:01 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (17.55%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,11 +1,17 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-
-        ans=[0] * (n+1)
-        ans[1]=1
-        ans[2]=2
-        for i in range(3,n+1):
-            ans[i]=ans[i-1]+ans[i-2]
-        return ans[-1]
+        dp=[-1] * n
+        def cal(i,n):
+            nonlocal dp
+            if i==n:
+                return 1
+            if i>n:
+                return 0
+            if dp[i] != -1:
+                return dp[i]
+            a=cal(i+1,n)
+            b=cal(i+2,n)
+            ans=a+b
+            dp[i]=ans
+            return ans
+        return cal(0,n)
