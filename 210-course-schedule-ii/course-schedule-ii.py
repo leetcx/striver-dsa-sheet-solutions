5035d1f8d349b556f4954ff135e5183c15caa80a
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-26 13:25:22 +0530

    Time: 7 ms (26.64%) | Memory: 20.5 MB (55.44%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,31 +1,28 @@
 from collections import deque
 class Solution:
     def findOrder(self, n: int, pr: List[List[int]]) -> List[int]:
-        indegree=[0] * n
-        adj=[[] for _ in range(n)]
-        q=deque()
         res=[]
-
+        adj=[[] for _ in range(n)] 
+        indegree=[0] * n
+        q=deque()
         for i in range(len(pr)):
             s=pr[i][0]
             d=pr[i][1]
             adj[d].append(s)
             indegree[s]+=1
         for i in range(len(indegree)):
-            if indegree[i] == 0:
+            if indegree[i]==0:
                 q.append(i)
         while q:
-            node=q.popleft()
-            res.append(node)
-            for i in range(len(adj[node])):
-                neigh=adj[node][i]
-                indegree[neigh]-=1
-                if indegree[neigh]==0:
-                    q.append(neigh)
-        if len(res) != n:
-            return []
+            p=len(q)
+            for i in range(p):
+                node=q.popleft()
+                res.append(node)
+                for i in range(len(adj[node])):
+                    neigh=adj[node][i]
+                    indegree[neigh]-=1
+                    if indegree[neigh]==0:
+                        q.append(neigh)
+        if len(res) !=n:
+            return []        
         return res
-
-        
-
-
