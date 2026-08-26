a20527d8770e0d9a6606adaf242c156bf815bbbe
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-26 12:30:05 +0530

    Time: 3 ms (36.60%) | Memory: 20.3 MB (36.62%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.py
+++ 113-path-sum-ii/path-sum-ii.py
@@ -7,23 +7,22 @@
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         ans=[]
-        temp=[]
         sum1=0
+        temp=[]
         def path(node):
-            nonlocal sum1
             nonlocal ans
+            nonlocal sum1
             nonlocal temp
             if node==None:
-                return
-            sum1=sum1+node.val
+                return []
+            sum1+=node.val
             temp.append(node.val)
             if node.left==None and node.right==None and sum1==targetSum:
                 ans.append(temp.copy())
             path(node.left)
             path(node.right)
-            temp.pop()
             sum1-=node.val
+            temp.pop()
         path(root)
         return ans
 
-            
