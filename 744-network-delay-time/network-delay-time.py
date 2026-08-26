b942537094376829f2c1e7d7f441bbf73e027750
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-26 11:24:14 +0530

    Time: 328 ms (86.07%) | Memory: 21.2 MB (54.68%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,31 +1,67 @@
 import heapq
-
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         adj=[[] for _ in range(n+1)]
         for i in range(len(times)):
-            s=times[i][0]
-            d=times[i][1]
-            wt=times[i][2]
-            adj[s].append((d,wt))
-        res=[(float('inf'))] * (n+1)
-
-   
+            src=times[i][0]
+            dest=times[i][1]
+            we=times[i][2]
+            adj[src].append((dest,we))
         q=[]
+        visited=[False] *(n+1)
+        res=[float('inf')] * (n+1)
         heapq.heappush(q,(0,k))
         res[k]=0
         while q:
             dist,node=heapq.heappop(q)
             if dist > res[node]:
                 continue
-            for i in range(len(adj[node])):
-                neigh=adj[node][i][0]
-                new=adj[node][i][1]
-                if   res[neigh] > new + dist:
-                    res[neigh]=new+dist
-                    heapq.heappush(q,(res[neigh],neigh))
-        p=max(res[1:])
-        if p != (float('inf')):
-            return p
-        return -1
-                
+            visited[node]=True
+            for p in range(len(adj[node])):
+                s=adj[node][p][0]
+                wt=adj[node][p][1]
+               
+                if visited[s]==False and res[s] > dist+ wt:
+                    res[s]= dist+wt
+                    heapq.heappush(q,(res[s],s))
+        g=max(res[1:])
+        if g == float('inf'):
+            return -1
+        return g
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
