58112ee7f26a49bfddad6310fe4c0c6f009d066e
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-26 12:58:55 +0530

    Time: 5 ms (22.87%) | Memory: 19.3 MB (41.52%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,21 +1,21 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        x=[-1,1,0,0]
-        y=[0,0,-1,1]
-        fresh=0
         m=len(grid)
         n=len(grid[0])
         q=deque()
-        visited=[[False]*n for _ in range(m)]
+        fresh=0
         time=0
+        x=[-1,1,0,0]
+        y=[0,0,-1,1]
+        visited=[[False]*n for _ in range(m)]
         for i in range(m):
-            for j in range(n):
+            for j in  range(n):
+                if grid[i][j]==1:
+                    fresh+=1
                 if grid[i][j]==2:
                     q.append((i,j))
                     visited[i][j]=True
-                if grid[i][j]==1:
-                    fresh+=1
         def valid(r,c,m,n):
             if r<0 or r>=m or c<0 or c>=n:
                 return False
@@ -24,20 +24,16 @@ class Solution:
             p=len(q)
             for i in range(p):
                 row,col=q.popleft()
-               
                 for k in range(4):
                     r=row+x[k]
                     c=col+y[k]
                     if valid(r,c,m,n) and visited[r][c]==False and grid[r][c]==1:
-                        q.append((r,c))
                         fresh-=1
-                        visited[r][c]= True
+                        visited[r][c]=True
+                        q.append((r,c))
             if q:
                 time+=1
-                    
-            
         if fresh>0:
             return -1
         return time
-
         
