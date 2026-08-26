c6a3d04eafa7ed0581b6309cd194b1e1cd0b8bad
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-26 13:46:47 +0530

    Time: 95 ms (5.06%) | Memory: 19.4 MB (95.74%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,27 +1,23 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        r=len(edges)        
-       
-        adj=[[] for _ in range(r+1)]
-        def dfs(node,target,visited):
-            if node==target:
-                return True
-            visited[node]=True
-            for k in range(len(adj[node])):
-                neigh=adj[node][k]
+        m=len(edges)
+        adj=[[] for _ in range(m+1)]
+        def dfs(i,j,visited,adj):
+            visited[i]=True
+            for k in range(len(adj[i])):
+                neigh=adj[i][k]
+                if neigh==j:
+                    return True
                 if visited[neigh]==False:
-                    if dfs(neigh,target,visited):
+                    if dfs(neigh,j,visited,adj):
                         return True
             return False
 
-        
-        for i in range(len(edges)):
-            visited=[False] *(r+1)
-            p=edges[i][0]
-            c=edges[i][1]
-            
-            if dfs(p,c,visited):
-                return [p,c]
-            adj[p].append(c)
-            adj[c].append(p)
-            
+        for i in range(m):
+            visited=[False] * (m+1)
+            s=edges[i][0]
+            d=edges[i][1]
+            if dfs(s,d,visited,adj)==True:
+                return [s,d]
+            adj[s].append(d)
+            adj[d].append(s)
