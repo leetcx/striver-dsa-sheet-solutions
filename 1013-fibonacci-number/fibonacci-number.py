1872602496b1a9cc07db9052c326bf5d6978244d
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-26 22:18:03 +0530

    Time: 41 ms (85.62%) | Memory: 19.2 MB (57.72%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,9 +1,17 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n>1:
-            return self.fib(n-2)+self.fib(n-1)
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
+        dp={}
+        def print(n):
+            nonlocal dp
+            if n==0 or n==1:
+                return n
+            if n in dp:
+                return dp.get(n)
+            a=print(n-1)
+            b=print(n-2)
+            ans=a+b
+            dp[n]=ans
+            return ans
+        return print(n)
+            
         
