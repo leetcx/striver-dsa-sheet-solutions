febcddb61985a5d103c1de042139120753f52953
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-27 22:57:10 +0530

    Time: 299 ms (85.46%) | Memory: 44.5 MB (45.58%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -2,22 +2,17 @@ class Solution:
     def longestCommonSubsequence(self, t1: str, t2: str) -> int:
         n=len(t1)
         m=len(t2)
-        dp=[[-1] *m for _ in range(n)]
-        T=0
-        def siu(i,j):
-            nonlocal T
-            if i == n or j==m:
-                return 0
-            if dp[i][j] != -1:
-                return dp[i][j]
-            if t1[i]==t2[j]:
-                dp[i][j]=1+siu(i+1,j+1)
-                return dp[i][j]   
-            else:
-                c1=siu(i+1,j)
-                c2=siu(i,j+1)
-                T=max(c1,c2)
-                dp[i][j]=T
-            return T
-        return siu(0,0)
-
+        dp=[[-1] *(m+1) for _ in range(n+1)]
+        for i in range(m+1):
+            dp[n][i] = 0
+        for i in range(n+1):
+            dp[i][m] = 0
+        for i in range(n-1,-1,-1):
+            for j in range(m-1,-1,-1):
+                if t1[i]==t2[j]:
+                    dp[i][j] =1+dp[i+1][j+1]
+                else:
+                    dp[i][j]=max((dp[i+1][j]),(dp[i][j+1]))
+        return dp[0][0]
+            
+        
