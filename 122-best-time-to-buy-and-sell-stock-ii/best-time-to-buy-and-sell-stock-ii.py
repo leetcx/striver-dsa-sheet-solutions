3964aae63a64723742bc19f9bd2222592b06c585
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-28 02:02:12 +0530

    Time: 25 ms (6.40%) | Memory: 22.8 MB (7.13%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,20 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
-        dp=[[-1] * 3 for _ in range(n)]
-        def best(i,k):
-            if i==n:
-                return 0
-            if dp[i][k] != -1:
-                return dp[i][k]
-            if k==2:
-                c=best(i+1,k-1) -prices[i]
-                c1=best(i+1,k)
-                dp[i][k]=max(c,c1)
-                return dp[i][k]
-            elif k==1:
-                c=best(i+1,k+1) + prices[i]
-                c1=best(i+1,k)
-                dp[i][k]=max(c,c1)
-                return dp[i][k]
-        return best(0,2)
+        dp=[[-1] * 3 for _ in range(n+1)]
+        k=2
+        for i in range(3):
+            dp[n][i]=0
+        for i in range(n-1,-1,-1):
+            for j in range(k,-1,-1):
+                if j==2:
+                    a=dp[i+1][j-1] - prices[i]
+                    b=dp[i+1][j]
+                    dp[i][j]=max(a,b)
+                elif j==1:
+                    d=dp[i+1][j+1] + prices[i]
+                    e=dp[i+1][j]
+                    dp[i][j]=max(d,e)
+        return dp[0][k]
+        
