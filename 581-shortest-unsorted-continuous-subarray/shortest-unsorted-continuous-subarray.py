9816c74ecbc203ff3c402c7e2dc5dc75b02db511
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-30 00:20:03 +0530

    Time: 7 ms (80.99%) | Memory: 20.6 MB (23.34%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.py
@@ -1,27 +1,34 @@
 class Solution:
     def findUnsortedSubarray(self, nums: List[int]) -> int:
-        p=sorted(nums)
-        low=0
-        high=len(nums)-1
+       
+        low=1
+        high=len(nums)-2
         found=False
         done=False
         point=0
         second=0
         for i in range(len(nums)):
-            if not found and p[low]!=nums[low]:
-                point=low
-                found=True
+            if low<len(nums) and nums[low]< nums[low-1]:
+                point=low-1
+                break
             else:
                 low+=1
-            if not done and p[high]!=nums[high]:
-                second=high
-                done=True
+        for i in range(len(nums)-2,-1,-1):
+            if high>=0 and nums[high] > nums[high+1]:
+                second=high+1
+                break
             else:
                 high-=1
-        if point ==0 and second==0:
+        if second==0 and point==0:
             return 0
-        subarr=nums[point:second+1]
-        g=len(subarr)
-        return g
-
-
+        mini=min(nums[point:second+1])
+        maxi=max(nums[point:second+1])
+        while point>0 and mini<nums[point-1]:
+            point-=1
+        while second<len(nums)-1 and maxi>nums[second+1]:
+            second+=1
+        p=(second+1)-point
+        return p
+        
+               
+            
