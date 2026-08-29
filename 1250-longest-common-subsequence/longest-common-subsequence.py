abecdc938e81b454b471520f9309f1746668d27b
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-29 12:59:52 +0530

    Time: 652 ms (23.76%) | Memory: 44.5 MB (45.43%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -2,17 +2,19 @@ class Solution:
     def longestCommonSubsequence(self, t1: str, t2: str) -> int:
         n=len(t1)
         m=len(t2)
-        dp=[[-1] *(m+1) for _ in range(n+1)]
-        for i in range(m+1):
-            dp[n][i] = 0
-        for i in range(n+1):
-            dp[i][m] = 0
-        for i in range(n-1,-1,-1):
-            for j in range(m-1,-1,-1):
-                if t1[i]==t2[j]:
-                    dp[i][j] =1+dp[i+1][j+1]
-                else:
-                    dp[i][j]=max((dp[i+1][j]),(dp[i][j+1]))
-        return dp[0][0]
-            
-        
+        dp=[[-1] * (m+1) for _ in range(n+1)]
+        def chill(i,j):
+            if i==n or j==m:
+                return 0
+            if dp[i][j] != -1:
+                return dp[i][j]
+            if t1[i] == t2[j]:
+                total=1+ chill(i+1,j+1)
+                dp[i][j]=total
+            else:
+                a=chill(i+1,j)
+                b=chill(i,j+1)
+                total=max(a,b)
+                dp[i][j]=total
+            return total
+        return chill(0,0)
