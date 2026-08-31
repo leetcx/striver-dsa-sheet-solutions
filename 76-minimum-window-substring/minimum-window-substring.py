8ccb6a7866d537226d453777c1779cc68092625e
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-31 13:07:54 +0530

    Time: 735 ms (5.00%) | Memory: 19.6 MB (73.92%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,36 +1,39 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        n=len(s)
-        p=len(t)
+        low=0
+        high=len(t)-1
         set1={}
-        for i in t:
-            if i in set1:
-                set1[i]+=1
-            else:
-                set1[i]=1
-        have=0
-        freq={}
-        minlen=float("inf")
-        left=0
+        set2={}
         ans=""
-        for i in range(n):
-            if s[i] in freq:
-                freq[s[i]]+=1
+        res=float('inf')
+        if len(t)>len(s):
+            return ""
+        for i in range(len(t)):
+            if t[i] in set1:
+                set1[t[i]]+=1
             else:
-                freq[s[i]]=1
-            if s[i] in set1 and freq[s[i]]==set1[s[i]]:
-                have+=1
-            while have==len(set1):
-                if i-left+1<minlen:
-                    minlen=i-left+1
-                    ans=s[left:i+1]
-                    
-                leftchar=s[left]
-                freq[leftchar]-=1
-                if leftchar in set1 and set1[leftchar]>freq[leftchar]:
-                    have-=1
-                left+=1
+                set1[t[i]]=1
+        for j in range(0,high+1):
+            if s[j] in set2:
+                set2[s[j]]+=1
+            else:
+                set2[s[j]]=1
+        while high< len(s) and low<len(s):
+            if all(set2.get(key, 0) >= value for key, value in set1.items()):
+                l1=high-low+1
+                if l1< res:
+                    ans=s[low:high+1]
+                    res=l1
+                
+                set2[s[low]]=set2.get(s[low],0)-1
+                if set2[s[low]] ==0:
+                    del set2[s[low]]
+                low+=1
+            else:
+                high+=1
+                if high>=len(s):
+                    break
+                set2[s[high]]=set2.get(s[high],0)+1
         return ans
-
 
 
