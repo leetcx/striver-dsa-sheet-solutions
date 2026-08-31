1d489b8207182ea14eaf6318b7d9aa41017f4b04
Sandipan Dutta <duttasandipan720@gmail.com>
2026-08-31 16:24:54 +0530

    Time: 15 ms (11.13%) | Memory: 20.4 MB (62.77%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,24 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        low=0
+        c=0
+        high=1
+        
+        while high<len(nums):
+            if nums[low]==0 and nums[high]==0:
+                high+=1
+            
+            elif nums[low]==0 and nums[high] !=0:
+                nums[low],nums[high] = nums[high],nums[low]
+                low+=1
+                high+=1
+            else:
+                low+=1
+                high+=1
+
+
+            
+        
