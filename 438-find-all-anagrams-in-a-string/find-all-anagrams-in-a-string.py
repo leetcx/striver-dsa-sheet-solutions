0e1ae8ca0ccd38f406a1a2f670d1d9f15737648b
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-01 03:35:54 +0530

    Time: 43 ms (53.69%) | Memory: 19.9 MB (16.12%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,36 +1,34 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        if len(p) > len(s):
-            return []
         low=0
+        high=0
+        t=len(p)
+        if t>len(s):
+            return []
         map1={}
         map2={}
-        k=len(p)
-        high=k-1
         ans=[]
-        for i in range(len(p)):
-            if p[i] not in map1:
-                map1[p[i]]=1
-            else:
+        for i in range(0,t):
+            if p[i] in map1:
                 map1[p[i]]+=1
-        for j in range(low,high+1):
-            if s[j] in map2:
-                map2[s[j]]+=1
             else:
-                map2[s[j]]=1
-        while high<len(s) :
-            if map1==map2:
+                map1[p[i]]=1
+        for high in range(0,len(s)):
+            if s[high] in map2:
+                map2[s[high]]+=1
+            else:
+                map2[s[high]]=1
+            
+            while high-low+1>t:
+                map2[s[low]]=map2.get(s[low],0)-1
+                if map2[s[low]]==0:
+                    del map2[s[low]]
+                low+=1
+            if map2==map1:
                 ans.append(low)
-            map2[s[low]]=map2.get(s[low],0) -1
-            if map2[s[low]] ==0:
-                del map2[s[low]]
-
-            low+=1
-            high+=1
-            if high==len(s):
-                break
-            map2[s[high]]=map2.get(s[high],0) +1
         return ans
 
 
-        
+
+            
+
