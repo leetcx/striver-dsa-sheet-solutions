3edf93059a3f0912d5a6b241670548689cb4a57a
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-01 12:16:11 +0530

    Time: 23 ms (80.15%) | Memory: 19.8 MB (78.68%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
@@ -6,16 +6,11 @@ class Solution:
         cost=0
         res=float('-inf')
         for high in range(len(s)):
-            if s[high] in seto:
-                seto[s[high]]+=1
-            else:
-                seto[s[high]]=1
+            
             cost+=abs(ord(s[high])-ord(t[high]))
             lou=high-low+1
             while cost>maxCost and low<high:
-                seto[s[low]]=seto.get(s[low],0)-1
-                if seto[s[low]]==0:
-                    del seto[s[low]]
+                
                 cost -= abs(ord(s[low]) - ord(t[low]))
                 low+=1
                 lou=high-low+1
