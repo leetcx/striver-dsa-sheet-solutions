efd47a4ad86c34055f4060f961adbac2262a2fde
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-02 21:32:55 +0530

    Time: 27 ms (40.35%) | Memory: 19.6 MB (12.57%) - LeetSync

---
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.py
@@ -0,0 +1,34 @@
+class Solution:
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        seti={}
+        for i in range(len(nums)):
+                if nums[i] in seti:
+                    seti[nums[i]]+=1
+                else:
+                    seti[nums[i]]=1
+        k=len(seti)
+        def atmost(k):
+            low=0
+            high=0
+            seto={}
+            count=0
+            c=0
+            
+            for high in range(len(nums)):
+                if nums[high] in seto:
+                    seto[nums[high]]+=1
+                else:
+                    seto[nums[high]]=1
+                while len(seto) > k:
+                    seto[nums[low]]=seto.get(nums[low],0)-1
+                    if seto[nums[low]]==0:
+                        del seto[nums[low]]
+                    low+=1
+                    
+                
+                
+               
+                count+=high-low+1
+            return count
+        return atmost(k)-atmost(k-1)
+                
