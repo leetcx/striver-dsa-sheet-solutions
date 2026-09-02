780b2b455a6d1f11ab610247fcfd7a0454e74daf
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-03 02:58:09 +0530

    Time: 271 ms (31.25%) | Memory: 31.1 MB (7.73%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.py
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.py
@@ -3,15 +3,17 @@ class Solution:
         nums.sort()
         low=0
         high=0
-        sum1=0
+        cost=0
         res=float('-inf')
+        sum1=0
         for high in range(len(nums)):
             sum1+=nums[high]
-            cost=nums[high] *(high-low+1)-sum1
-            while cost>k:
+            cost=nums[high] *(high-low+1) -sum1
+            while cost>k and low<high:
                 sum1-=nums[low]
+                
                 low+=1
-                cost=nums[high] *(high-low+1)-sum1
+                cost=nums[high] *(high-low+1) - sum1
+                
             res=max(res,high-low+1)
         return res
-            
