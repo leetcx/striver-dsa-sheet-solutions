7d438b828aa3636319da18ec7ef6b7a2d67dc6a1
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-03 01:21:18 +0530

    Time: 195 ms (34.42%) | Memory: 25.9 MB (34.61%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -2,24 +2,18 @@ class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         low=0
         high=0
-        seto={}
+        set1={}
         res=float('-inf')
         for high in range(len(fruits)):
-            if fruits[high] in seto:
-                seto[fruits[high]]+=1
+            if fruits[high] in set1:
+                set1[fruits[high]]+=1
             else:
-                seto[fruits[high]]=1
-            lou=high-low+1
-            while len(seto)>2:
-                seto[fruits[low]]=seto.get(fruits[low],0)-1
-                if  seto[fruits[low]]==0:
-                    del seto[fruits[low]]
+                set1[fruits[high]]=1
+            while len(set1) >2:
+                set1[fruits[low]]=set1.get(fruits[low],0)-1
+                if set1[fruits[low]]==0:
+                    del set1[fruits[low]]
                 low+=1
-                lou=high-low+1
-            if len(seto) <= 2:
-                lou=high-low+1
-                res=max(res,lou)
-        if res==float('-inf'):
-            return 0
+            res=max(res,high-low+1)
         return res
             
