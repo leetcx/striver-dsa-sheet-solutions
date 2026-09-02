5f6f0cc683c118bf4d17edb62c7a0388e0758035
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-03 02:04:11 +0530

    Time: 139 ms (47.32%) | Memory: 21.9 MB (68.72%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -3,24 +3,20 @@ class Solution:
         def atmost(k):
             low=0
             high=0
-            seto={}
+            set1={}
             count=0
-            c=0
             for high in range(len(nums)):
-                if nums[high] in seto:
-                    seto[nums[high]]+=1
+                if nums[high] in set1:
+                    set1[nums[high]]+=1
                 else:
-                    seto[nums[high]]=1
-                while len(seto) > k:
-                    seto[nums[low]]=seto.get(nums[low],0)-1
-                    if seto[nums[low]]==0:
-                        del seto[nums[low]]
+                    set1[nums[high]]=1
+                while len(set1) > k:
+                    set1[nums[low]]=set1.get(nums[low],0)-1
+                    if set1[nums[low]]==0:
+                        del set1[nums[low]]
                     low+=1
-                    
-                
-                
-               
                 count+=high-low+1
             return count
         return atmost(k)-atmost(k-1)
+                
                 
