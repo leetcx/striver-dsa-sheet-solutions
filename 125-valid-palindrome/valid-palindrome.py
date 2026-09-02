a131876255f2ad2332a0baa0ffc52057f71a2736
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-03 00:14:00 +0530

    Time: 12 ms (13.28%) | Memory: 19.7 MB (39.22%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,13 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        new=""
+        news=""
+        
         for i in range(len(s)):
-            if ('a' <= s[i] <= 'z') or ('A' <=s[i] <= 'Z') or ('0' <= s[i] <= '9'):
-                new+=s[i].lower()
+            if s[i].isalpha() or s[i].isdigit():
+                news += s[i].lower()
+
+        
         low=0
-        high=len(new)-1
+        high=len(news)-1
         while low<high:
-            if new[low] != new[high]:
+            if news[low] != news[high]:
                 return False
             low+=1
             high-=1
