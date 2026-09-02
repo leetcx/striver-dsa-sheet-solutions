48d7cf8dae94afdaf8c60aaab4d8d271486f0c41
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-02 13:12:36 +0530

    Time: 139 ms (47.36%) | Memory: 22 MB (40.29%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -1,27 +1,26 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        low=0
-        high=0
-        seto={}
-        count=0
-        c=0
-        for high in range(len(nums)):
-            if nums[high] in seto:
-                seto[nums[high]]+=1
-            else:
-                seto[nums[high]]=1
-            while len(seto) > k:
-                seto[nums[low]]=seto.get(nums[low],0)-1
-                if seto[nums[low]]==0:
-                    del seto[nums[low]]
-                low+=1
-                c=0
-            if len(seto)==k:
-                
-                while seto[nums[low]] >1:
-                    seto[nums[low]]-=1
+        def atmost(k):
+            low=0
+            high=0
+            seto={}
+            count=0
+            c=0
+            for high in range(len(nums)):
+                if nums[high] in seto:
+                    seto[nums[high]]+=1
+                else:
+                    seto[nums[high]]=1
+                while len(seto) > k:
+                    seto[nums[low]]=seto.get(nums[low],0)-1
+                    if seto[nums[low]]==0:
+                        del seto[nums[low]]
                     low+=1
-                    c+=1
-                count+=c+1
-        return count
+                    
+                
+                
+               
+                count+=high-low+1
+            return count
+        return atmost(k)-atmost(k-1)
                 
