16c6dcb6e5c281f0d9d929fa4c90d7b4663c6dda
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-04 00:50:54 +0530

    Time: 112 ms (28.60%) | Memory: 19.5 MB (17.16%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -1,21 +1,19 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-       
-        set2={}
         low=0
         count=0
+        set1={}
         for high in range(len(s)):
-            
-            if s[high] in set2:
-                set2[s[high]]+=1
+            if s[high] in set1:
+                set1[s[high]]+=1
             else:
-                set2[s[high]]=1
-            while len(set2)==3:
+                set1[s[high]]=1
+            while len(set1)==3:
                 count+=len(s)-high
-                set2[s[low]]=set2.get(s[low],0)-1
-                if set2[s[low]]==0:
-                    del set2[s[low]]
+                
+                set1[s[low]]=set1.get(s[low],0)-1
+                if set1[s[low]]==0:
+                    del set1[s[low]]
                 low+=1
         return count
-
-
+                   
