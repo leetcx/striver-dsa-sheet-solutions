413f03e7babcd272ad8ac85e05b4f05053b45662
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-03 23:52:25 +0530

    Time: 132 ms (32.78%) | Memory: 19.6 MB (52.34%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -3,21 +3,17 @@ class Solution:
         low=0
         high=0
         set1={}
-        c=len(s)
         res=float('-inf')
         for high in range(len(s)):
             if s[high] in set1:
                 set1[s[high]]+=1
             else:
                 set1[s[high]]=1
-            p = (high - low + 1) - max(set1.values())
-            while p>k :
+            p=max(set1.values())
+            while (high-low+1)-p>k and low<=high:
                 set1[s[low]]=set1.get(s[low],0)-1
                 if set1[s[low]]==0:
                     del set1[s[low]]
                 low+=1
-                p=(high - low + 1) -max(set1.values())
             res=max(res,high-low+1)
         return res
-
-
