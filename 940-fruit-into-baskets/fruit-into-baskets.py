785dd66c9374c0d0e89cc54175ef1d1ffae93e96
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-04 00:08:36 +0530

    Time: 213 ms (14.63%) | Memory: 25.7 MB (89.37%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,7 +1,6 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         low=0
-        high=0
         set1={}
         res=float('-inf')
         for high in range(len(fruits)):
@@ -9,7 +8,7 @@ class Solution:
                 set1[fruits[high]]+=1
             else:
                 set1[fruits[high]]=1
-            while len(set1) >2:
+            while len(set1)>2:
                 set1[fruits[low]]=set1.get(fruits[low],0)-1
                 if set1[fruits[low]]==0:
                     del set1[fruits[low]]
