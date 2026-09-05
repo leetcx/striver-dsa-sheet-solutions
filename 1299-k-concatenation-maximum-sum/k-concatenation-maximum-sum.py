c2c763d201f229a69b5e49c8ccab35ab1976679c
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-05 14:18:38 +0530

    Time: 63 ms (19.08%) | Memory: 31.8 MB (54.05%) - LeetSync

--- 1299-k-concatenation-maximum-sum/k-concatenation-maximum-sum.py
+++ 1299-k-concatenation-maximum-sum/k-concatenation-maximum-sum.py
@@ -1,28 +1,31 @@
 class Solution:
     def kConcatenationMaxSum(self, arr: List[int], k: int) -> int:
+        best=arr[0]
+        ans=arr[0]
+        for i in range(1,len(arr)):
+            best=max(arr[i],best+arr[i])
+            ans=max(ans,best)
         
-        best=0
-        res=0
-        for i in range(len(arr)):
-            best=max(best + arr[i],arr[i])
-            res=max(res,best)
+        if ans<0:
+            return 0
         if k==1:
-            return res
-        total=sum(arr)
-        bestpre=float('-inf')
+            return ans
         sum1=0
+        res=float('-inf')
         for i in range(len(arr)):
             sum1+=arr[i]
-            bestpre=max(bestpre,sum1)
-        bestsuff=float('-inf')
-        sum1=0
+            res=max(res,sum1)
+        sum2=0
+        rise=float('-inf')
         for i in range(len(arr)-1,-1,-1):
-            sum1+=arr[i]
-            bestsuff=max(bestsuff,sum1)
-        if total<=0:
-            return max(res,bestsuff+bestpre)
+            sum2+=arr[i]
+            rise=max(rise,sum2)
+
+       
+        if sum(arr)>0:
+            ans=max(ans,res+rise+(sum(arr)*(k-2))) % (10**9 + 7)
+
+            return ans
         else:
-            return max(res, bestsuff + (k-2)*total + bestpre) % (10**9 + 7)
-            
-            
-            
+            return max(ans,res+rise)
+        
