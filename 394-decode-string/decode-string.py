700403be853c70118867645c02d1055092db193e
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-08 15:22:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (41.02%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,23 +1,27 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         st=[]
+        curr=""
         num=0
-        word=""
         for i in range(len(s)):
             if s[i]=='[':
-                st.append((word,num))
-                word=""
+                st.append((curr,num))
+                curr=""
                 num=0
                 continue
             if s[i]==']':
-                prev,n=st.pop()
-                word=prev+word*n
-                continue
-            if s[i].isdigit():
-                num=num*10+int(s[i])
+                if st:
+                    prev,n=st.pop()
+                    curr=prev+n*curr
+                    prev=curr
+                    n=num
+                else:
+                    return curr
             else:
-                word+=s[i]
-        return word
-            
-            
-            
+                if s[i].isdigit():
+                    num=num*10+int(s[i])
+                    continue
+                else:
+                    curr+=s[i]
+        return curr
+       
