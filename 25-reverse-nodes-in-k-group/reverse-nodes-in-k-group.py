bb14852743bb7022b18b8a04da0cb2133f0c1446
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-09 23:06:50 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (15.04%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -4,24 +4,42 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        temp=head
-        count=0
-
-        while count<k:
-            if temp==None:
-                return head
-            temp=temp.next
-            count+=1
-        curr=head
+    def reverse(self,left,k):
+        curr=left
         prev=None
-        cnt=0
-
-        while cnt<k:
-            next=curr.next
+        while curr and k>0:
+            temp=curr.next
             curr.next=prev
             prev=curr
-            curr=next
-            cnt+=1
-        head.next=self.reverseKGroup(curr,k)
-        return prev
+            curr=temp
+            k-=1
+        return
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        left=head
+        prevleft=None
+        res=None
+        while True:
+            right=left
+            for i in range(0,k-1):
+                if right==None:
+                    break
+                right=right.next
+            if right:
+                nextleft=right.next
+                self.reverse(left,k)
+                if prevleft:
+                    prevleft.next=right
+                prevleft=left
+                if res==None:
+                    res=right
+                left=nextleft
+            else:
+                if res==None:
+                    res=left
+                if prevleft:
+                    prevleft.next=left
+                break
+
+        return res                    
+
+
