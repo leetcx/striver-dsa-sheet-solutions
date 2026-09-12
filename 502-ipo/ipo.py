3222066115c6a1ac7ceb53d2b0b915ee2632c9a9
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-12 13:23:05 +0530

    Time: 280 ms (60.06%) | Memory: 46.4 MB (40.37%) - LeetSync

--- 502-ipo/ipo.py
+++ 502-ipo/ipo.py
@@ -1,26 +1,27 @@
-import heapq
+minheap=[]
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        temp=[]
-        for i in range (len(profits)):
-            a=profits[i]
-            b=capital[i]
-            temp.append((b,a))
+        n=len(profits) 
+        temp=[]   
+        minheap=[]
+        for i in range(n):
+            temp.append((capital[i],profits[i]))
         temp.sort()
+        if w < temp[0][0]:
+            return w
         idx=0
-        maxheap=[]
         while k:
-            while idx<len(profits):
-                if temp[idx][0]>w:
+            
+            while idx <n:
+                if w >=temp[idx][0]:
+                    heapq.heappush(minheap,-temp[idx][1])
+                    idx+=1
+                else:
                     break
-                heapq.heappush(maxheap,-temp[idx][1])
-                idx+=1
-            if maxheap:
-                p=heapq.heappop(maxheap)
-                w+=-(p)
+            if minheap:
+                t=-heapq.heappop(minheap)  
+                w+=t
             else:
                 return w
             k-=1
-        return w
-
-                
+        return w  
