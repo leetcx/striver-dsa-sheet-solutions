430460714d8ac8b13172765f222706d669289456
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-14 01:43:47 +0530

    Time: 203 ms (14.80%) | Memory: 20.5 MB (81.73%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,18 +1,25 @@
 class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        n=len(piles)
+    def minEatingSpeed(self, piles: List[int], ho: int) -> int:
+        def find(mid):
+            h=0
+            for i in piles:
+                t=i%mid
+                d=i//mid
+                if t>0:
+                    h+=d+1
+                else:
+                    h+=d
+            return h
         low=1
         high=max(piles)
-
-        while low<high:
+        res=0
+        while low<=high:
             mid=(low+high)//2
-            hours=0
-
-            for i in piles:
-                hours+=(i+mid-1)//mid
-
-            if hours<=h:
-                high=mid
+            c=find(mid)
+            if c<=ho:
+                res=mid
+                high=mid-1
             else:
                 low=mid+1
-        return high
+        return res
+        
