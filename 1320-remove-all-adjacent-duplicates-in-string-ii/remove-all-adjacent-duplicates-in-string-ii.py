4e36324a18ef29e4a38aa77fbacdec19ea23c565
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-13 12:13:12 +0530

    Time: 67 ms (8.01%) | Memory: 22.7 MB (93.84%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
@@ -5,21 +5,19 @@ class Solution:
             if not st:
                 st.append((s[i],1))
                 continue
-            if st[-1][0] != s[i]:
+            if st and st[-1][0] != s[i]:
                 st.append((s[i],1))
                 continue
-            if st[-1][1] < k-1:
+            if st and st[-1][0]==s[i] and st[-1][1] < (k-1):
                 st[-1]=(st[-1][0],st[-1][1]+1)
                 continue
-            else:
-                st.pop()
-        ans=[]
-        while st:
-            d=st[-1][0]
-            z=st[-1][1]
-            an=z * d
-            ans.append(an)
             st.pop()
-        ans.reverse()
-        return "".join(ans)
+        new=[]
+        while st:
+            p,q=st.pop()
+            new.append(p*q)
+        new.reverse()
+        return "".join(new)
 
+
+            
