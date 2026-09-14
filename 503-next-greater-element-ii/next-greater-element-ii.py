222b4e38c12c47fca9ced0f7a982a74fed7032a2
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-14 16:20:21 +0530

    Time: 19 ms (83.54%) | Memory: 21 MB (58.47%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -1,14 +1,14 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         nums+=nums
-        d=len(nums)
-        temp=[-1] * d
+        p=len(nums)
+        res=[-1] * p
         st=[]
-        for i in range(len(nums)-1,-1,-1):
+        for i in range(p-1,-1,-1):
             while st and st[-1] <= nums[i]:
                 st.pop()
             if st:
-                temp[i]=st[-1]
+                res[i]=st[-1]
             st.append(nums[i])
-        m=d//2
-        return temp[:m]
+        z=p//2
+        return res[:z]
