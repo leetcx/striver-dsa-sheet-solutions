16e0c3eb655692e1adefe045c602e158e8d5c6fc
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-14 17:14:27 +0530

    Time: 168 ms (8.14%) | Memory: 25 MB (46.30%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -5,14 +5,13 @@ class Solution:
             odd=0
             count=0
             for high in range(len(nums)):
-                if nums[high] %2==1:
+                if nums[high]%2==1:
                     odd+=1
                 while odd>k and low<=high:
-                    if nums[low]%2==1:
+                    if nums[low] %2==1:
+                        
                         odd-=1
                     low+=1
                 count+=high-low+1
             return count
-        return atmost(k)-atmost(k-1)
-        
-        
+        return atmost(k) - atmost(k-1)
