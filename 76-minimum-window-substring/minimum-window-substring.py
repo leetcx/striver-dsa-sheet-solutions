2c881f57aea755e37c66f70833da8e0b47c0c1cd
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-14 22:15:53 +0530

    Time: 752 ms (5.03%) | Memory: 19.6 MB (73.86%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,32 +1,28 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        low=0
-        high=0
+        if len(t) > len(s):
+            return ""
         set1={}
-        have=0
-        g=len(t)
-        set2={}
-        new=""
+        g=""
+        for i in t:
+            if i in set1:
+                set1[i]+=1
+            else:
+                set1[i]=1
+        low=0
         res=float('inf')
-        for i in range(g):
-            if t[i] in set2:
-                set2[t[i]]+=1
-            else:
-                set2[t[i]]=1
+        set2={}
         for high in range(len(s)):
-            if s[high] in set1:
-                set1[s[high]]+=1
+            if s[high] in set2:
+                set2[s[high]]+=1
             else:
-                set1[s[high]]=1
-            while all(set1.get(k, 0) >= v for k, v in set2.items()):
-
-                if high-low+1<res:
-
-                    new=s[low:high+1]
+                set2[s[high]]=1
+            while all(set2.get(k, 0) >= v for k, v in set1.items()) and low<=high:
+                if res>high-low+1:
                     res=high-low+1
-                set1[s[low]]=set1.get(s[low],0)-1
-                if set1[s[low]]==0:
-                    del set1[s[low]]
+                    g=s[low:high+1]
+                set2[s[low]]=set2.get(s[low],0)-1
+                if set2[s[low]]==0:
+                    del set2[s[low]]
                 low+=1
-        return new
-                   
+        return g
