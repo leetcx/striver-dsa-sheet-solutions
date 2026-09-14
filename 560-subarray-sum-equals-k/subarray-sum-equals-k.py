4305c71aa8a4815764dc5c1432b5795c36957a7a
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-14 16:07:57 +0530

    Time: 31 ms (72.12%) | Memory: 21.9 MB (56.70%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,15 +1,19 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        low=0
-        high=0
-        sum1=0
         set1={0:1}
-        ans=0
-        for high in range(len(nums)):
-            sum1+=nums[high]
-            pref=sum1-k
-            if pref in set1:
-                ans+=set1[pref]
-            set1[sum1] = set1.get(sum1, 0) + 1
-        return ans
+        sum1=0
+        count=0
+        for i in range(len(nums)):
+            sum1+=nums[i]
+            prev=sum1-k
+            if prev in set1:
+                count+=set1[prev]
+                
+                
             
+            
+            if sum1 in set1:
+                set1[sum1]+=1
+            else:
+                set1[sum1]=1
+        return count
