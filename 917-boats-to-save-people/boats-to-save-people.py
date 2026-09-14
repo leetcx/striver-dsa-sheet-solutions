fcdb9f458da4c2e616043ea3765316dbe3ed773f
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-15 02:20:05 +0530

    Time: 52 ms (43.56%) | Memory: 25.2 MB (36.50%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -1,15 +1,18 @@
 class Solution:
-    def numRescueBoats(self, people: List[int], limit: int) -> int:
-        people.sort()
+    def numRescueBoats(self, nums: List[int], limit: int) -> int:
+        nums.sort()
         low=0
-        high=len(people) -1
-        boat=0
+        high=len(nums)-1
+        count=0
         while low<=high:
-            if people[low]+people[high]<=limit:
-                boat+=1
-                low+=1
+            sum1=nums[low]+nums[high]
+            if sum1>limit:
+                boats=sum1//limit
+                count+=boats
                 high-=1
             else:
-                boat+=1
+                count+=1
+                low+=1
                 high-=1
-        return boat
+        return count
+            
