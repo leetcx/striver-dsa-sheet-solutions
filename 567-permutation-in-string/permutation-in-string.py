9c6c52955add82b624b13910d47350e7b37173d1
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-15 19:27:55 +0530

    Time: 16 ms (66.71%) | Memory: 19.5 MB (19.23%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,31 +1,32 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        t=len(s1)
-        map1={}
-        map2={}
-        if t>len(s2):
+        set1={}
+        p=len(s1)
+        if p> len(s2):
             return False
-        for i in range(0,t):
-            if s1[i] in map1:
-                map1[s1[i]]+=1
+        for i in s1:
+            if i in set1:
+                set1[i]+=1
             else:
-                map1[s1[i]]=1
+                set1[i]=1
+        set2={}
+        for i in range(p):
+            if s2[i] in set2:
+                set2[s2[i]]+=1
+            else:
+                set2[s2[i]]=1
+        if set1==set2:
+            return True
         low=0
-        high=t-1
-        for j in range(low,high+1):
-            if s2[j] in map2:
-                map2[s2[j]] +=1
-            else:
-                map2[s2[j]] =1
-        while high < len(s2):
-            if map1==map2:
-                return True
-            map2[s2[low]]=map2.get(s2[low],0)-1
-            if map2[s2[low]]==0:
-                del map2[s2[low]]
+        high=p
+        while high<len(s2):
+            set2[s2[low]]=set2.get(s2[low],0)-1
+            if set2[s2[low]]==0:
+                del set2[s2[low]]
             low+=1
+            set2[s2[high]]=set2.get(s2[high],0)+1
             high+=1
-            if high>=len(s2):
-                break
-            map2[s2[high]]=map2.get(s2[high],0)+1
+            if set1==set2:
+                return True
         return False
+            
