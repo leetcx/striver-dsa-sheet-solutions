6c42e2949b7db4250831798faac01ddadbafc83b
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-15 21:22:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (79.41%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -1,18 +1,22 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        ans=[]
-        odd=[]
+        
         low=0
         high=len(nums)-1
         
         while low<=high:
-            if nums[low]%2==0:
-                ans.append(nums[low])
-                
+            if nums[low]%2==1 and nums[high]%2==0:
+                nums[low],nums[high]=nums[high],nums[low]
+                low+=1
+                high-=1
             else:
-                odd.append(nums[low])
-            low+=1
-        nums[:]=ans+odd
+                if nums[low]%2==0 and nums[high]%2==0:
+                    low+=1
+                else:
+                    high-=1
 
+               
+            
+        
         return nums
 
