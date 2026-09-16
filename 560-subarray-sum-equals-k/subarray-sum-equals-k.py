525727143d1de334f5f115486ace679edf147daa
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-16 20:02:00 +0530

    Time: 32 ms (58.25%) | Memory: 21.9 MB (56.89%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -8,10 +8,6 @@ class Solution:
             prev=sum1-k
             if prev in set1:
                 count+=set1[prev]
-                
-                
-            
-            
             if sum1 in set1:
                 set1[sum1]+=1
             else:
