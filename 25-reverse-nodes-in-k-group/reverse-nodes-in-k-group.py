643c6494ab071ad541d693942185d980ae1a58d1
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-17 16:43:09 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (82.57%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -4,42 +4,46 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverse(self,left,k):
+    def reverse(self,left,t):
         curr=left
         prev=None
-        while curr and k>0:
+        while curr and t>0:
             temp=curr.next
             curr.next=prev
             prev=curr
             curr=temp
-            k-=1
-        return
-    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+            t-=1
+        return prev,left
+        
+    def reverseKGroup(self, head: ListNode | None, k: int) -> ListNode | None:
         left=head
         prevleft=None
         res=None
         while True:
             right=left
-            for i in range(0,k-1):
-                if right==None:
+            for i in range(k-1):
+                if right:
+                    right=right.next
+                else:
                     break
-                right=right.next
             if right:
                 nextleft=right.next
-                self.reverse(left,k)
+                newhead,tail=self.reverse(left,k)
                 if prevleft:
-                    prevleft.next=right
-                prevleft=left
-                if res==None:
-                    res=right
+                    prevleft.next=newhead
+                prevleft=tail
+                prevleft.next=nextleft
+
                 left=nextleft
+                
+                if res==None:
+                    res=newhead
             else:
                 if res==None:
                     res=left
-                if prevleft:
-                    prevleft.next=left
+                else:
+                    if prevleft:
+                        prevleft.next=left
                 break
-
-        return res                    
-
-
+        return res
+                
