fd89b3bf01839901f47524f6e6c85f0fd6e0dd41
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-18 16:18:15 +0530

    Time: 43 ms (16.52%) | Memory: 20 MB (76.10%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,16 +1,14 @@
 class Solution:
-    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        def atmost(goal):
-            count=0
-            low=0
+    def numSubarraysWithSum(self, nums: list[int], k: int) -> int:
+        def atmost(k):
             sum1=0
+            low=0
+            count=0
             for high in range(len(nums)):
                 sum1+=nums[high]
-                while sum1>goal and low<=high:
+                while sum1>k and low<=high:
                     sum1-=nums[low]
                     low+=1
-            
                 count+=high-low+1
-               
             return count
-        return atmost(goal)-atmost(goal-1)
+        return atmost(k)-atmost(k-1)
