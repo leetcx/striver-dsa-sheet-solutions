8e0387f6aa1110bf692377152c5f056e48c4867e
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-24 13:41:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (25.51%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,19 +1,25 @@
 class Solution:
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+    def subsetsWithDup(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
         ans=[]
         temp=[]
-
-        def backtrack(i):
-            ans.append(temp.copy())
-
-            for j in range(i,len(nums)):
-                if j>i and nums[j]==nums[j-1]:
-                    continue
-                temp.append(nums[j])
-                backtrack(j+1)
-                temp.pop()
-        backtrack(0)
-        return ans
-
+        def organ(i):
+            nonlocal temp
+            nonlocal ans
+            if i==len(nums):
+                ans.append(temp.copy())
+                return
+            temp.append(nums[i])
+            organ(i+1)
+            temp.pop()
+            while i+1<len(nums) and nums[i]==nums[i+1]:
+                i+=1
+            organ(i+1)
                
+            
+        organ(0)
+        return ans
+            
+                
+        
+
