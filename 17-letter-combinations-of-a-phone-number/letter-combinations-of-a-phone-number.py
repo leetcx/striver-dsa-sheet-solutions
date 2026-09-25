24995e3c3484e449809152e367845c0c7d322c9c
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-25 19:08:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (79.14%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,19 +1,48 @@
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        temp=[]
-        n=len(digits)
-        phone = {"2": "abc","3": "def","4": "ghi","5": "jkl","6": "mno","7": "pqrs","8": "tuv", "9": "wxyz"}
-        ans=[]
-        n=len(digits)
+    def letterCombinations(self, digits: str) -> list[str]:
+        if not digits:
+            return []
 
-        def back(i):
-            if i==n:
+        phone = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        ans = []
+        temp = []
+
+        def solve(i):
+            if i == len(digits):
                 ans.append("".join(temp))
                 return
-            strop=phone[digits[i]]
-            for j in range(len(strop)):
-                temp.append(strop[j])
-                back(i+1)
+
+            # First nested level
+            for ch1 in phone[digits[i]]:
+                temp.append(ch1)
+
+                # Second nested level
+                if i + 1 < len(digits):
+                    for ch2 in phone[digits[i + 1]]:
+                        temp.append(ch2)
+
+                        # Backtracking handles everything after these 2
+                        if i + 2 < len(digits):
+                            solve(i + 2)
+                        else:
+                            ans.append("".join(temp))
+
+                        temp.pop()
+                else:
+                    ans.append("".join(temp))
+
                 temp.pop()
-        back(0)
+
+        solve(0)
         return ans
+        
