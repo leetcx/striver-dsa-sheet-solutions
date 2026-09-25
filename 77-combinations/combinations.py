c6b6ff4e526e04b9c3130e071354b181674b0d4a
Sandipan Dutta <duttasandipan720@gmail.com>
2026-09-25 16:01:47 +0530

    Time: 139 ms (18.94%) | Memory: 61.4 MB (55.51%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,20 +1,19 @@
 class Solution:
     def combine(self, n: int, k: int) -> list[list[int]]:
-        copy=[0] * n
-        for i in range(n):
-            copy[i]=i+1
-        ans=[]
         temp=[]
-        def solve(j,p):
+        ans=[]
+        def all(i):
+            nonlocal ans
             nonlocal temp
-            nonlocal ans 
-            nonlocal copy
-            if j==k:
+            if len(temp)==k:
                 ans.append(temp.copy())
-                return 
-            for i in range(p,len(copy)):
-                temp.append(copy[i])
-                solve(j+1,i+1)
-                temp.pop()
-        solve(0,0)
+                return
+            if i>=n+1:
+                return
+           
+            temp.append(i)
+            all(i+1)
+            temp.pop()
+            all(i+1)
+        all(1)
         return ans
